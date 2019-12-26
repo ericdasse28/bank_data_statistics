de1825372f51c5cacbdb756ee3b9c41ccc99655c
ericdasse28 <christ.gououi@gmail.com>
2019-12-26 19:45:42 +0100

    Definition of a function to detect the most common words in the 'libelle' column
    That function makes a list of all the words in the column and uses the
    Counter library to retrieve the most common of them all

--- python_analysis.py
+++ python_analysis.py
@@ -1,5 +1,20 @@
 import pandas as pd
+from collections import Counter
 
 
-data = pd.read_csv("data/operations.csv")
+# There are columns representing dates. We must indicate them when creating
+# the dataframe using the parse_dates argument
+data = pd.read_csv("data/operations.csv", parse_dates=[1, 2])
 print(data)
+
+
+# Creating a function to retrieve the most frequent words within the 'libelle'
+# column to better track consumption habits
+def most_common_words(labels):
+    words = []
+    for lab in labels:
+        words += lab.split(" ")
+
+    counter = Counter(words)
+    for word in counter.most_common(100):
+        print(word)
